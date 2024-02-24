e8ec29d76d180ec2c0256cebe944f30b853dda50
Joseph <yusufmaliktr@gmail.com>
2024-02-25 01:50:24 +0300

    menü bar ın bir kısmı tamamlandı

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// Pencere boyutu değiştiğinde çağrılacak fonksiyon
+/*function boyutdegistirme() {
+    var menüSection = document.getElementById("menü");
+    var windowWidth = window.innerWidth;
+
+    // Pencere genişliğine bağlı olarak padding değerlerini ayarla
+    if (windowWidth < 768) {
+        menüSection.style.padding = "0  100px"; // Küçük ekranlar için
+    } else {
+        menüSection.style.padding = "0 400px"; // Diğer durumlar için
+    }
+}
+
+// Sayfa yüklendiğinde ve pencere boyutu değiştiğinde boyutdegistirme() fonksiyonunu çağır
+window.onload = boyutdegistirme;
+window.onresize = boyutdegistirme;
+// Pencere boyutu değiştiğinde çağrılacak fonksiyon
+function adjustMenu() {
+    var menüSection = document.getElementById("menü");
+    var windowWidth = window.innerWidth;
+    var menüWidth = menüSection.offsetWidth;
+
+    // Menüyü ortalamak için gerekli boşluk hesaplanır
+    var padding = (windowWidth - menüWidth) / 2;
+
+    // Padding değerlerini ayarla
+    menüSection.style.paddingLeft = padding + "px";
+    menüSection.style.paddingRight = padding + "px";
+}
+
+// Sayfa yüklendiğinde ve pencere boyutu değiştiğinde adjustMenu() fonksiyonunu çağır
+window.onload = adjustMenu;
+window.onresize = adjustMenu;*/
